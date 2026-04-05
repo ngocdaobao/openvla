e91e313c43b7049d973cfc5c389463955cf4b440
ngocdaobao <dbaongoc2312@gmail.com>
2026-04-05 22:02:54 +0700

    add videoprism

---
+++ videoprism-main/videoprism/encoders_test.py
@@ -0,0 +1,375 @@
+# Copyright 2026 VideoPrism Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for encoder modules."""
+
+from absl.testing import absltest
+from absl.testing import parameterized
+import chex
+import jax
+from jax import numpy as jnp
+import numpy as np
+from videoprism import encoders
+
+
+class EncodersTest(parameterized.TestCase):
+
+  @chex.variants(with_jit=True, without_jit=True)
+  def test_embedding_layer(self):
+    num_classes, dim, input_shape = 8, 10, (5, 20)
+    npy_input = np.random.randint(0, num_classes, input_shape).astype('int32')
+    inputs = jnp.asarray(npy_input)
+    prng_key = jax.random.PRNGKey(seed=123)
+    emb_layer = encoders.Embedding(
+        name='emb_lookup',
+        num_classes=num_classes,
+        input_dim=dim,
+        scale_sqrt_depth=True,
+    )
+
+    @self.variant
+    def var_fn():
+      return emb_layer.init_with_output(prng_key, inputs)
+
+    outputs, params = var_fn()
+    self.assertLen(jax.tree_util.tree_flatten(params)[0], 1)
+    self.assertEqual(outputs.shape, input_shape + (dim,))
+
+  @chex.variants(with_jit=True, without_jit=True)
+  def test_positional_embedding_layer(self):
+    seq_len, dim = 8, 10
+    prng_key = jax.random.PRNGKey(seed=123)
+    emb_layer = encoders.PositionalEmbedding(
+        name='pos_emb',
+        embedding_dim=dim,
+        min_timescale=10,
+        max_timescale=20,
+    )
+
+    @self.variant
+    def var_fn():
+      return emb_layer.init_with_output(prng_key, seq_len)
+
+    outputs, params = var_fn()
+    self.assertEmpty(jax.tree_util.tree_flatten(params)[0])
+    self.assertEqual(outputs.shape, (1, seq_len, dim))
+
+  @chex.variants(with_jit=True, without_jit=True)
+  def test_trainable_positional_embedding_layer(self):
+    seq_len, dim = 8, 10
+    prng_key = jax.random.PRNGKey(seed=123)
+    emb_layer = encoders.TrainablePositionalEmbedding(
+        name='pos_emb',
+        max_seq_length=seq_len,
+        embedding_dim=dim,
+        lookup_style='matmul',
+    )
+
+    @self.variant
+    def var_fn():
+      return emb_layer.init_with_output(prng_key, seq_len)
+
+    outputs, params = var_fn()
+    self.assertLen(jax.tree_util.tree_flatten(params)[0], 1)
+    self.assertEqual(outputs.shape, (1, seq_len, dim))
+
+  @chex.variants(with_jit=True)
+  @parameterized.product(
+      scan=[True, False],
+      train=[True, False],
+  )
+  def test_vision_transformer(self, scan: bool, train: bool):
+    batch_size, seq_len, dim = 1, 6, 4
+    np_inputs = np.random.normal(1.0, 0.5, [batch_size, seq_len, dim]).astype(
+        'float32'
+    )
+    inputs = jnp.asarray(np_inputs)
+    prng_key = jax.random.PRNGKey(seed=123)
+    vit = encoders.VisionTransformer(
+        name='vit',
+        num_tfm_layers=2,
+        mlp_dim=4,
+        num_heads=2,
+        scan=scan,
+    )
+
+    @self.variant
+    def var_fn():
+      return vit.init_with_output(prng_key, inputs, train=train)
+
+    outputs, params = var_fn()
+    self.assertLen(jax.tree_util.tree_flatten(params)[0], 16 if scan else 32)
+    self.assertEqual(outputs.shape, (batch_size, seq_len, dim))
+
+  @chex.variants(with_jit=True)
+  @parameterized.named_parameters(
+      ('train', False, True, False, False),
+      ('scan', True, False, False, False),
+      ('scan_and_train', True, True, False, False),
+      ('return_intermediate', True, False, True, False),
+      ('use_frame_paddings', True, False, False, True),
+  )
+  def test_factorized_encoder(
+      self,
+      scan: bool,
+      train: bool,
+      return_intermediate: bool,
+      use_frame_paddings: bool,
+  ):
+    batch_size, num_frames, image_size, patch_size, dim = 1, 4, 16, 4, 8
+    np_inputs = np.random.normal(
+        0.0,
+        0.1,
+        [batch_size, num_frames, image_size, image_size, 3],
+    ).astype('float32')
+    inputs = jnp.asarray(np_inputs)
+
+    frame_paddings = None
+    if use_frame_paddings:
+      np_frame_paddings = np.zeros((batch_size, num_frames), dtype='float32')
+      np_frame_paddings[:, num_frames // 2 :] = 1
+      frame_paddings = jnp.asarray(np_frame_paddings)
+
+    prng_key = jax.random.PRNGKey(seed=123)
+    enc = encoders.FactorizedEncoder(
+        name='enc',
+        patch_size=patch_size,
+        pos_emb_shape=(16, 16, 16),
+        model_dim=dim,
+        num_spatial_layers=2,
+        num_temporal_layers=2,
+        num_heads=2,
+        mlp_dim=4,
+        atten_logit_cap=50.0,
+        scan=scan,
+    )
+
+    @self.variant
+    def var_fn():
+      return enc.init_with_output(
+          prng_key,
+          inputs,
+          train=train,
+          return_intermediate=return_intermediate,
+          frame_paddings=frame_paddings,
+      )
+
+    (embeddings, outputs), params = var_fn()
+
+    self.assertLen(jax.tree_util.tree_flatten(params)[0], 40 if scan else 72)
+    self.assertEqual(
+        embeddings.shape,
+        (batch_size, num_frames * (image_size // patch_size) ** 2, dim),
+    )
+    if return_intermediate:
+      self.assertEqual(
+          outputs['spatial_features'].shape,
+          (batch_size, num_frames * (image_size // patch_size) ** 2, dim),
+      )
+    else:
+      self.assertEmpty(outputs)
+
+  @chex.variants(with_jit=True)
+  @parameterized.named_parameters(
+      ('train', True, False),
+      ('return_intermediate', False, True),
+  )
+  def test_factorized_video_classifier(
+      self, train: bool, return_intermediate: bool
+  ):
+    batch_size, num_frames, image_size, patch_size, dim = 1, 4, 16, 4, 8
+    np_inputs = np.random.normal(
+        0.0,
+        0.1,
+        [batch_size, num_frames, image_size, image_size, 3],
+    ).astype('float32')
+    inputs = jnp.asarray(np_inputs)
+
+    encoder_params = dict(
+        patch_size=patch_size,
+        pos_emb_shape=(16, 16, 16),
+        model_dim=dim,
+        num_spatial_layers=2,
+        num_temporal_layers=2,
+        num_heads=2,
+        mlp_dim=4,
+        atten_logit_cap=50.0,
+        scan=True,
+    )
+    prng_key = jax.random.PRNGKey(seed=123)
+    classifier = encoders.FactorizedVideoClassifier(
+        name='classifier',
+        encoder_params=encoder_params,
+        num_classes=10,
+    )
+
+    @self.variant
+    def var_fn():
+      return classifier.init_with_output(
+          prng_key, inputs, train=train, return_intermediate=return_intermediate
+      )
+
+    (logits, outputs), params = var_fn()
+    self.assertLen(jax.tree_util.tree_flatten(params)[0], 54)
+    self.assertEqual(logits.shape, (batch_size, 10))
+    if return_intermediate:
+      self.assertEqual(
+          outputs['spatial_features'].shape,
+          (batch_size, num_frames * (image_size // patch_size) ** 2, dim),
+      )
+      self.assertEqual(
+          outputs['spatiotemporal_features'].shape,
+          (batch_size, num_frames * (image_size // patch_size) ** 2, dim),
+      )
+      self.assertEqual(outputs['global_embeddings'].shape, (batch_size, dim))
+    else:
+      self.assertEmpty(outputs)
+
+  @chex.variants(with_jit=True)
+  @parameterized.named_parameters(
+      ('train', False, True),
+      ('scan', True, False),
+      ('scan_and_train', True, True),
+  )
+  def test_text_encoder(self, scan: bool, train: bool):
+    batch_size, seq_len, vocab_size, dim = 1, 10, 20, 8
+    np_inputs = np.random.randint(0, vocab_size, [batch_size, seq_len]).astype(
+        'int32'
+    )
+    inputs = jnp.asarray(np_inputs)
+    np_paddings = np.zeros([batch_size, seq_len], dtype='float32')
+    np_paddings[:, seq_len // 2 :] = 1
+    paddings = jnp.asarray(np_paddings)
+
+    prng_key = jax.random.PRNGKey(seed=123)
+    enc = encoders.TextEncoder(
+        name='enc',
+        vocabulary_size=vocab_size,
+        num_class_tokens=1,
+        model_dim=dim,
+        num_layers=2,
+        mlp_dim=4,
+        num_heads=2,
+        atten_logit_cap=50.0,
+        scan=scan,
+    )
+
+    @self.variant
+    def var_fn():
+      return enc.init_with_output(prng_key, inputs, paddings, train=train)
+
+    outputs, params = var_fn()
+    self.assertLen(jax.tree_util.tree_flatten(params)[0], 20 if scan else 36)
+    self.assertEqual(outputs.shape, (batch_size, seq_len + 1, dim))
+
+  @chex.variants(with_jit=True)
+  @parameterized.named_parameters(
+      ('train', False, True, False),
+      ('scan_and_train', True, True, False),
+      ('return_intermediate', True, False, True),
+      (
+          'selectively_return_intermediate',
+          True,
+          False,
+          {'spatial_features', 'frame_embeddings'},
+      ),
+  )
+  def test_factorized_video_clip(
+      self, scan: bool, train: bool, return_intermediate: bool
+  ):
+    batch_size, num_frames, image_size, patch_size, dim = 1, 4, 16, 4, 8
+    np_inputs = np.random.normal(
+        0.0,
+        0.1,
+        [batch_size, num_frames, image_size, image_size, 3],
+    ).astype('float32')
+    inputs = jnp.asarray(np_inputs)
+
+    seq_len, vocab_size = 10, 20
+    np_text_token_ids = np.random.randint(
+        0, vocab_size, [batch_size, seq_len]
+    ).astype('int32')
+    text_token_ids = jnp.asarray(np_text_token_ids)
+    np_text_paddings = np.zeros([batch_size, seq_len], dtype='float32')
+    np_text_paddings[:, seq_len // 2 :] = 1
+    text_paddings = jnp.asarray(np_text_paddings)
+
+    prng_key = jax.random.PRNGKey(seed=123)
+    net = encoders.FactorizedVideoCLIP(
+        name='net',
+        patch_size=patch_size,
+        pos_emb_shape=(16, 16, 16),
+        num_spatial_layers=2,
+        num_temporal_layers=2,
+        mlp_dim=4,
+        num_auxiliary_layers=1,
+        vocabulary_size=vocab_size,
+        enable_causal_atten=True,
+        num_unimodal_layers=2,
+        norm_policy='pre',
+        model_dim=dim,
+        num_heads=2,
+        atten_logit_cap=50.0,
+        scan=scan,
+    )
+
+    @self.variant
+    def var_fn():
+      return net.init_with_output(
+          prng_key,
+          inputs=inputs,
+          text_token_ids=text_token_ids,
+          text_paddings=text_paddings,
+          train=train,
+          normalize=True,
+          return_intermediate=return_intermediate,
+      )
+
+    (video_embeddings, text_embeddings, outputs), params = var_fn()
+    self.assertLen(jax.tree_util.tree_flatten(params)[0], 88 if scan else 136)
+    self.assertEqual(video_embeddings.shape, (batch_size, dim))
+    self.assertEqual(text_embeddings.shape, (batch_size, dim))
+    if not return_intermediate:
+      self.assertEmpty(outputs)
+    else:
+      if return_intermediate is True:  # pylint: disable=g-bool-id-comparison
+        self.assertEqual(
+            set(outputs.keys()),
+            {
+                'frame_embeddings',
+                'spatial_features',
+                'spatiotemporal_features',
+            },
+        )
+      else:
+        self.assertEqual(set(outputs.keys()), set(return_intermediate))
+
+      if 'spatial_features' in outputs:
+        self.assertEqual(
+            outputs['spatial_features'].shape,
+            (batch_size, num_frames * (image_size // patch_size) ** 2, dim),
+        )
+      if 'spatiotemporal_features' in outputs:
+        self.assertEqual(
+            outputs['spatiotemporal_features'].shape,
+            (batch_size, num_frames * (image_size // patch_size) ** 2, dim),
+        )
+      if 'frame_embeddings' in outputs:
+        self.assertEqual(
+            outputs['frame_embeddings'].shape, (batch_size, num_frames, dim)
+        )
+
+
+if __name__ == '__main__':
+  absltest.main()
---
+++ videoprism-main/videoprism/models_test.py
@@ -0,0 +1,120 @@
+# Copyright 2026 VideoPrism Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for VideoPrism models."""
+
+from absl.testing import absltest
+from absl.testing import parameterized
+import jax
+from jax import numpy as jnp
+import numpy as np
+from videoprism import models
+from videoprism import tokenizers
+
+
+class ModelsTest(parameterized.TestCase):
+
+  @parameterized.parameters(
+      ('videoprism_public_v1_base', True),
+      ('videoprism_public_v1_large', True),
+      ('videoprism_public_v1_giant', False),
+  )
+  def test_has_model(self, model_name, exists):
+    self.assertEqual(models.has_model(model_name), exists)
+
+  @parameterized.parameters(8, 16)
+  def test_videoprism(self, num_frames):
+    batch_size = 1
+    np_inputs = np.random.normal(
+        0.0, 0.1, [batch_size, num_frames, 288, 288, 3]
+    ).astype('float32')
+    inputs = jnp.asarray(np_inputs)
+    prng_key = jax.random.PRNGKey(seed=123)
+
+    mdl = models.videoprism_v1_base()
+    mdl_params = mdl.init(prng_key, inputs, train=False)
+
+    @jax.jit
+    def forward_fn(mdl_inputs):
+      return mdl.apply(mdl_params, mdl_inputs, train=False)
+
+    embeddings, _ = forward_fn(inputs)
+    self.assertEqual(embeddings.shape, (batch_size, num_frames * 16**2, 768))
+
+  def test_videoprism_lvt(self):
+    batch_size, num_frames = 1, 16
+    np_inputs = np.random.normal(
+        0.0, 0.1, [batch_size, num_frames, 288, 288, 3]
+    ).astype('float32')
+    inputs = jnp.asarray(np_inputs)
+    np_text_token_ids = np.random.randint(
+        0, 32_000, [batch_size, models.TEXT_MAX_LEN]
+    ).astype('int32')
+    text_token_ids = jnp.asarray(np_text_token_ids)
+    np_text_paddings = np.zeros(
+        [batch_size, models.TEXT_MAX_LEN], dtype='float32'
+    )
+    np_text_paddings[:, models.TEXT_MAX_LEN // 2 :] = 1
+    text_paddings = jnp.asarray(np_text_paddings)
+    prng_key = jax.random.PRNGKey(seed=123)
+
+    mdl = models.videoprism_lvt_v1_base()
+    mdl_params = mdl.init(
+        prng_key, inputs, text_token_ids, text_paddings, train=False
+    )
+
+    @jax.jit
+    def forward_fn(mdl_inputs, mdl_text_token_ids, mdl_text_paddings):
+      return mdl.apply(
+          mdl_params,
+          mdl_inputs,
+          mdl_text_token_ids,
+          mdl_text_paddings,
+          train=False,
+      )
+
+    vision_embeddings, text_embeddings, _ = forward_fn(
+        inputs, text_token_ids, text_paddings
+    )
+    self.assertEqual(vision_embeddings.shape, (batch_size, 768))
+    self.assertEqual(text_embeddings.shape, (batch_size, 768))
+
+  def test_tokenize_texts(self):
+    import os
+    spm_path = os.path.join(
+        os.path.dirname(__file__), 'assets', 'testdata', 'test_spm.model'
+    )
+    model = tokenizers.SentencePieceTokenizer(spm_path)
+    ids, paddings = models.tokenize_texts(
+        model,
+        ['blah', 'blah blah', 'blah blah blah'],
+        max_length=6,
+        add_bos=False,
+        canonicalize=False,
+    )
+    np.testing.assert_array_equal(
+        ids,
+        [
+            [80, 180, 60, 0, 0, 0],
+            [80, 180, 60, 80, 180, 60],
+            [80, 180, 60, 80, 180, 60],
+        ],
+    )
+    np.testing.assert_array_equal(
+        paddings, [[0, 0, 0, 1, 1, 1], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+    )
+
+
+if __name__ == '__main__':
+  absltest.main()
