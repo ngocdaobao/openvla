fd0682875d0397a274d187c6113adbf64b3f9b43
Hoang Nguyen <dbaongoc2312@gmail.com>
2026-04-13 14:20:13 +0000

    edit wandb_entity

--- vla-scripts/finetune.py
+++ vla-scripts/finetune.py
@@ -104,7 +104,7 @@ class FinetuneConfig:
 
     # Tracking Parameters
     wandb_project: str = "openvla"                                  # Name of W&B project to log to (use default!)
-    wandb_entity: str = "stanford-voltron"                          # Name of entity to log under
+    wandb_entity: str = "ngoc-db-vinuniversity"                          # Name of entity to log under
     run_id_note: Optional[str] = None                               # Extra note for logging, Weights & Biases
 
     # fmt: on
